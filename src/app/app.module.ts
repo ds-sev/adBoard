ec5eb85f84cb8a63aa054bbbe69346739cb56e66
Dmitry Litvinenko <ds-sev@ya.ru>
2023-03-29 21:02:04 +0300

    edit angular_json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdViewComponent } from './modules/ad-core/pages/ad-view/ad-view/ad-view.component';
+import { HeaderComponent } from './shared/components/header/header/header.component';
+import { CardComponent } from './shared/components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdViewComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/modules/ad-core/pages/ad-view/ad-view/ad-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-core-view',
+  templateUrl: './ad-view.component.html',
+  styleUrls: ['./ad-view.component.css']
+})
+export class AdViewComponent {
+
+}
