f22418572e2f3d375f082935dc211acf7d4e04a5
Dmitry Litvinenko <ds-sev@ya.ru>
2023-03-30 22:39:23 +0300

    feature: create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Routes, RouterModule } from '@angular/router'
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'
+import { RecommendationsComponent } from './pages/recommendations/recommendations.component'
+import { NewAdComponent } from './pages/new-ad/new-ad.component'
+import { AdDetailsComponent } from './pages/ad-details/ad-details.component'
+import { AccountSettingsComponent } from './pages/account-settings/account-settings.component'
+import { MyAdsComponent } from './pages/my-ads/my-ads.component'
 
 const routes: Routes = [
   {path: '', component: AppComponent},
+  {path: 'recommendations', component: RecommendationsComponent},
+  {path: 'my-ads', component: MyAdsComponent},
+  {path: 'new-ad', component: NewAdComponent},
+  {path: 'ad/:id', component: AdDetailsComponent},
+  {path: 'account-settings', component: AccountSettingsComponent},
 
+  {path: '**', redirectTo: '', component: RecommendationsComponent},
 ]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,16 @@ import { AdViewComponent } from './modules/ad-registry/pages/ad-view/ad-view/ad-
 import { CardComponent } from './shared/components/card/card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { NavigationComponent } from './components/navigation/navigation.component'
+import { CascadeSelectModule } from 'primeng/cascadeselect'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { ButtonModule } from 'primeng/button';
+import { RecommendationsComponent } from './pages/recommendations/recommendations.component';
+import { MyAdsComponent } from './pages/my-ads/my-ads.component';
+import { NewAdComponent } from './pages/new-ad/new-ad.component';
+import { AccountSettingsComponent } from './pages/account-settings/account-settings.component'
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
@@ -15,10 +24,20 @@ import { FooterComponent } from './components/footer/footer.component'
     HeaderComponent,
     CardComponent,
     FooterComponent,
+    NavigationComponent,
+    RecommendationsComponent,
+    MyAdsComponent,
+    NewAdComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     BrowserModule,
-    RouterOutlet
+    RouterOutlet,
+    CascadeSelectModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
